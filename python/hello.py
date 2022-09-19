57c2400a7a273cf8b6fadce8ae561575c7b6971c
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-09-19 09:22:37 +0000

    python

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,11 @@
-print("hello")
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("200x100")
+
+label = tk.Label(text="LABEL")
+btn = tk.Button(text="push")
+
+label.pack()
+btn.pack()
+tk.mainloop()

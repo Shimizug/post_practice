cd68f53be9cec175973d30276b45b4bda0374fcb
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-09-27 03:03:12 +0000

    ruby

--- ruby/hello.rb
+++ ruby/hello.rb
@@ -1,4 +1,27 @@
-a = 3.0 / 2
-p a
-b = a * 2
-puts b
+#2-1
+puts 2 + 3
+puts "#{2 * 2 * 3.14}cm"
+
+#2-2
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+
+#2-3
+coffee = 300
+esp = 100
+puts "コーヒー：#{coffee}円"
+puts "合計：#{coffee + esp + esp}円"
+coffee = 400
+puts "コーヒー：#{coffee}円"
+puts "合計：#{coffee + esp + esp}円"
+
+#2-6
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+
+#2-7
+total = 300 * 2
+puts total

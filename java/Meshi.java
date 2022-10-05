7885df0fb8b95e0de8ddd0fc7e42ddc4405f8575
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-05 07:42:05 +0000

    java Meshi修正

--- java/Meshi.java
+++ java/Meshi.java
@@ -1,14 +1,14 @@
-Public class Meshi{
+public class Meshi{
   public static void main(String[] args){
-    int isHungry = new java.util.Random().nextInt(1);
+    int isHungry = new java.util.Random().nextInt(2);
     String food = "お米";
-    System.out.prinntln("こんにちは");
+    System.out.println("こんにちは");
     if (isHungry == 0){
       System.out.println("お腹がいっぱいです");
     }else{
       System.out.println("はらぺこです");
       System.out.println(food + "いただきます。");
     }
-    System.out.println("ごちそうさまでした")
+    System.out.println("ごちそうさまでした");
   }
 }

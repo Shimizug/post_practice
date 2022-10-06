e3e29ae8a657f155dce0c55801db8a745c7c971d
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-06 01:36:54 +0000

    Chapter04.java

---
+++ java/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04{
+  public static void main(String[] args){
+    String name = "清水隼也";
+    System.out.println(name);
+    
+    name = "巧言令色鮮し仁";
+    System.out.println(name);
+    final int LUCKY = 5;
+    System.out.println(LUCKY);
+  }
+}

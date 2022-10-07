a8c69df495b37b6a25f80206333665ff6bce68da
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-07 01:00:09 +0000

    Capter05.java

---
+++ java/Chapter05.java
@@ -0,0 +1,16 @@
+public class Chapter05{
+  public static void main(String[] args){
+    long x = 1000L;
+    double y = 10.1d;
+    boolean z = true;
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    Integer a = 100;
+    Float b = 1.1b;
+    Character c = "あ";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+  }
+}

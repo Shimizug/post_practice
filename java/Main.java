4f2cfa6f1edf4db1f0159e1f5ab422140c0888c6
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-14 14:08:29 +0000

    java

--- java/Main.java
+++ java/Main.java
@@ -1,8 +1,21 @@
 public class Main{
   public static void main(String[] args){
-    int a = 3;
-    int b = 5;
-    int c = a * b;
-    System.out.println(c);
+  System.out.println("Hello World");   //1-1
+  int x = 11;
+  System.out.println("x=" + x ); //1-2
+  x = 13;
+  int y = 17;
+  System.out.println("x=" + x + ",y=" + y); //1-3
+  x = 13 + 17;
+  System.out.println(x); //1-4
+  System.out.println(13 * 17); //1-5
+  x = 7;
+  x *= 3;
+  System.out.println(x);
+  x /= 2;
+  System.out.println(x);//1-6
+  
   }
+  
 }
+

4f611d6fb64fb9a43fcb8abf774362657cf6dd7e
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-16 12:07:49 +0000

    java

--- java/Main.java
+++ java/Main.java
@@ -1,9 +1,12 @@
-import java.io.*;
-
 public class Main{
   public static void main(String[] args){
-    BufferesReader br = new BufferesReader(
-      new InputStreamReader(system.in) );
-      int n = Integer.parseInt(br.readLine());
+    int isHungry = new java.util.Random().nextInt(1);
+    String food = "rice";
+    System.out.println("こんにちは")
+    if (isHungry == 0){
+      System.out.println("お腹がいっぱいです");
+    }else{
+      System.out.println("はらぺこです");
+    }
   }
 }

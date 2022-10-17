5793c0045a52e2fe00824f2017bad91fb3993f31
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-17 12:56:25 +0000

    java

--- java/Main.java
+++ java/Main.java
@@ -4,15 +4,11 @@ public class Main{
     h.name = "ミナト";
     h.hp = 100;
     
-    Matango ml = new Matango();
-    ml.hp = 50;
-    ml.suffix = 'A';
-    
     System.out.println("勇者" + h.name + "の誕生");
     h.sit(9);
     h.slip();
     h.sit(5);
     h.run();
-    ml.ran();
+  
   }
 }

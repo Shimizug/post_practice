f1c295da0f6b9ebf9b643dc411fc1c10ad275206
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-17 02:12:02 +0000

    java method

--- java/Main.java
+++ java/Main.java
@@ -1,12 +1,32 @@
 public class Main{
   public static void main(String[] args){
-    int isHungry = new java.util.Random().nextInt(1);
-    String food = "rice";
-    System.out.println("こんにちは")
-    if (isHungry == 0){
-      System.out.println("お腹がいっぱいです");
-    }else{
-      System.out.println("はらぺこです");
-    }
+    introduceOneself();
+    email("あい", "class", "king");
+    email("あい", "class");
+    System.out.println(calcTriangleArea(10.0, 5.0));
+    System.out.println(calcCircleArea(5.0));
+  }
+  public static void introduceOneself(){
+    String name = "AKi";
+    int age = 22;
+    double height = 134.4;
+    char eto = '辰';
+    System.out.println(name + age + height + eto);
+  }
+  
+  public static void email(String title, String address, String text){
+    System.out.println(address);
+    System.out.println(title);
+    System.out.println(text);
+  }
+  public static void email(String address, String text){
+    System.out.println(address);
+    System.out.println(text);
+  }
+  public static double calcTriangleArea(double bottom, double height){
+    return bottom * height / 2;
+  }
+  public static double calcCircleArea(double radius){
+    return radius * radius * 3.14;
   }
 }

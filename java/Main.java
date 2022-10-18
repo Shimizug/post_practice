5350f24314689ee7be1067f3946a3cc468fd5b3a
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-18 13:45:17 +0000

    java

--- java/Main.java
+++ java/Main.java
@@ -3,7 +3,7 @@ public class Main{
     Hero h = new Hero("ミナト");
     h.hp = 100;
     Hero h2 = new Hero();
-    SYstem.out.println(h2.name);
+    System.out.println(h2.name);
     
     System.out.println("勇者" + h.name + "の誕生");
     h.sit(9);

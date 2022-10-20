fc4f710eab391261a8baad976ef5749d4c854707
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-20 04:48:51 +0000

    javaの復習、最小公倍数に変更

--- java/Output.java
+++ java/Output.java
@@ -2,9 +2,13 @@ public class Output{
   public static void main(String[] args){
     for (int i = 2; i <= 20; i++){
       if (i % 15 == 0){
-        System.out.println("かつ丼\￥850");
+        System.out.println("かつ丼￥8５0");
       }else if (i % 3 == 0){
         System.out.println("かつ丼");
+      }else if (i % 5 == 0){
+        System.out.println("￥8５0");
+      } else {
+        System.out.println(i);
       }
     }
   }

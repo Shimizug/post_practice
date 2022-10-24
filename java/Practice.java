92d415d2a11300ba2552363b1c79bd7a1e978086
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-24 08:23:24 +0000

    1-4

--- java/Practice.java
+++ java/Practice.java
@@ -1,8 +1,7 @@
 public class Practice{
   public static void main(String[] args){
-    //int 型の変数 x、y に数値 13、17 を代入し、x、y の値を“x=13,y=17”のように表示するプログラムを作成しなさい。
-    int x = 13;
-    int y = 17;
-    System.out.println("x=" + x + ",y=" + y);
+    //int 型の変数 x に数値 13 と 17 の和を代入し、x の値を表示するプログラムを作成しなさい。
+    int x = 13 + 17;
+    System.out.println("x=" + x);
   }
 }

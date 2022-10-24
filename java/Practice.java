82d5e18de790fb8315f79512b78940fa516dfc65
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-24 08:19:40 +0000

    1-1

--- java/Practice.java
+++ java/Practice.java
@@ -1,6 +1,7 @@
 public class Practice{
   public static void main(String[] args){
-    //“Hello World”と表示するプログラムを作成しなさい
-    System.out.println("Hello World");
+    //int 型の変数 x に数値 11 を代入し、x の値を“x=11”のように表示するプログラムを作成しなさい。
+    int x = 11;
+    System.out.println("x=" + x);
   }
 }

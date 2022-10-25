b905129e827e922af51040e2eaf343ae774b3082
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-25 00:31:01 +0000

    1-6.java

--- java/Practice.java
+++ java/Practice.java
@@ -1,6 +1,15 @@
 public class Practice{
   public static void main(String[] args){
-    //数値 13 と 17 の積を表示するプログラムを作成しなさい。ただし、変数を使用しないこと。
-    System.out.println(13 * );
+    /*int 型の変数 x に数値 7 を代入する。
+    変数 x の値を 3 倍にする。
+    変数 x の値を表示する。
+    変数 x の値を半分（1/2）にする。
+    変数 x の値を表示する
+    */
+    int x = 7;
+    x *= 3;
+    System.out.println(x);
+    x /= 2;
+    System.out.println(x);
   }
 }

a9a22309a2f6e601d0dce2a215037fbb50a0a5d7
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-25 05:29:53 +0000

    1-8.java

--- java/Practice.java
+++ java/Practice.java
@@ -1,12 +1,9 @@
 public class Practice{
   public static void main(String[] args){
-    //int 型の変数 x、y に任意の数値を代入し、x の値を y に、y の値を x に入れ替えて x と y の値を表示するプログラムを作成しなさい
-    int x = 1;
-    int y = 2;
-    int z = x;
-    x = y;
-    y = z;
-    
-    System.out.println(x + "と" + y)
+    //iint 型の変数 x、y に数値 19、23 を代入し、その積を変数 z に代入して z の値を表示するプログラムを作成しなさい。
+    int x = 19;
+    int y = 23;
+    int z = x * y;
+    System.out.println(z);
   }
 }

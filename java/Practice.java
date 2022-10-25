3e78eb2156a1a3937b6f4511dc7d3ff6ed5a8581
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-10-25 00:28:42 +0000

    1-5.java

--- java/Practice.java
+++ java/Practice.java
@@ -1,7 +1,6 @@
 public class Practice{
   public static void main(String[] args){
-    //int 型の変数 x に数値 13 と 17 の和を代入し、x の値を表示するプログラムを作成しなさい。
-    int x = 13 + 17;
-    System.out.println("x=" + x);
+    //数値 13 と 17 の積を表示するプログラムを作成しなさい。ただし、変数を使用しないこと。
+    System.out.println(13 * );
   }
 }
